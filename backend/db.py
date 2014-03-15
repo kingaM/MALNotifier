736047add67c475620e9c103defdf2b9b42b99be
michboon <michboon@email>
2014-03-15 15:34:17 +0000

    DBHelper

---
+++ backend/db.py
@@ -0,0 +1,29 @@
+# EXAMPLE
+#
+# from db import DBHelper
+# db = DBHelper()
+# print db.retrieveData("SELECT * FROM `users`")
+
+import MySQLdb as mdb
+
+class DBHelper:
+
+    try:
+        con = mdb.connect(host = '127.0.0.1', user = 'root', passwd = 'root', db = 'MALNotifier')
+    except Exception, e:
+        print e
+
+    def executeQuery(self, query):
+        cursor = DBHelper.con.cursor()
+        cursor.execute(query)
+        id = cursor.lastrowid
+        cursor.close()
+        DBHelper.con.commit()
+        return id
+
+    def retrieveData(self, query):
+        cursor = DBHelper.con.cursor()
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        cursor.close()
+        return rows
