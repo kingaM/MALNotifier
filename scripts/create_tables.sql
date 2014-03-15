d198942fac3893f4e3cf325e9e2458aa36bc7ade
Kinga Mrugala <kinga_2009@yahoo.com>
2014-03-15 15:26:33 +0100

    Initial database

---
+++ scripts/create_tables.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE IF NOT EXISTS MALNotifier;
+
+USE MALNotifier;
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL, 
+    `facebook` INT,
+    `mal` VARCHAR(255) NOT NULL,
+    UNIQUE (`mal`),
+    UNIQUE (`facebook`),
+    UNIQUE (`email`),
+    PRIMARY KEY(`id`)
+); 
+
+CREATE TABLE IF NOT EXISTS `shows` (
+    `showId` INT NOT NULL AUTO_INCREMENT,
+    `sequel` INT,
+    PRIMARY KEY(`showId`)
+);
+
+CREATE TABLE IF NOT EXISTS `titles` (
+    `titleId` INT NOT NULL AUTO_INCREMENT,
+    `showId` INT NOT NULL,
+    `title` VARCHAR(2000),
+    PRIMARY KEY(`showId`),
+    FOREIGN KEY (`showId`) REFERENCES `shows` (`showId`) ON DELETE CASCADE ON UPDATE CASCADE
+);
