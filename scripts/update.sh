3b1e4402755bfeb548385ddd1a22d8db0561bbec
michboon <michboon@email>
2014-03-16 04:58:31 +0000

    fix to updater

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,7 +3,11 @@ rm -f ./master.zip
 wget https://github.com/michboon/MALNotifier/archive/master.zip
 unzip master.zip
 sudo rm -rf /var/www/*
+sudo rm -rf /var/scripts/*
+sudo rm -rf /var/backend/*
 sudo cp -r ./MALNotifier-master/www/* /var/www
+sudo cp -r ./MALNotifier-master/scripts/* /var/scripts
+sudo cp -r ./MALNotifier-master/backend/* /var/backend
 sudo chmod +x ./MALNotifier-master/scripts/update.sh
 
 cd ./MALNotifier-master/scripts
