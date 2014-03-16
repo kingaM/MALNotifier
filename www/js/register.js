cc611696bb7bb79c1c25892a2f69149f79aeaaa0
michboon <michboon@email>
2014-03-16 09:21:36 +0000

    Uploading MAL xmls

--- www/js/register.js
+++ www/js/register.js
@@ -1,14 +1,47 @@
+var files;
+
 $(document).ready(function() {
     console.log("clicked");
+
+    $('input[type=file]').on('change', function (event) {
+        files = event.target.files;
+    });
+
     $("#register-from").submit(function(e) {
         e.preventDefault();
         console.log("clicked");
         values= {};
         values['email'] = $("#register-email").val();
         values['mal'] = $("#register-mal").val();
-        $.post("/register.php",values, function(data) {
-            console.log(data);
-            window.location.href = "/shows.html?" + values['mal'];
-        });
+        uploadFiles(e);
     });   
 });
+
+function uploadFiles(event) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    var data = new FormData();
+    $.each(files, function(key, value) {
+        data.append(key, value);
+    });
+
+    data.append("email", $("#register-email").val());
+    data.append("mal", $("#register-mal").val());
+    
+    $.ajax({
+        url: '/register.php',
+        type: 'POST',
+        data: data,
+        cache: false,
+        processData: false,
+        contentType: false,
+        success: function(data) {
+            console.log(data)
+            window.location.href = "/shows.html?" + $("#register-mal").val();
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.error("ERROR: " + textStatus);
+        }
+    });
+}
--- www/register.php
+++ www/register.php
@@ -2,7 +2,27 @@
 
 require_once('database.php');
 
-function addUser($email, $mal, $facebook = null) {
+function addUser($email, $mal, $xml, $facebook = null) {
+    $allowedExts = array("xml");
+    $temp = explode(".", $xml["name"]);
+    $extension = end($temp);
+    if (($xml["type"] == "text/xml") && in_array($extension, $allowedExts)) {
+        if ($xml["error"] > 0) {
+            echo "Return Code: " . $xml["error"] . "<br>";
+        }
+        else {
+            if (file_exists("../backend/" . $mal . ".xml")) {
+                echo $mal . " already exists. ";
+            }
+            else {
+                move_uploaded_file($xml["tmp_name"], "../backend/" . $mal . ".xml");
+                echo "Stored in: " . "../backend/" . $mal;
+            }
+        }
+    } else {
+        echo "Invalid file";
+    }
+
     $db = new DatabaseHelper();
     $db->execute("INSERT INTO 
         users(email, mal, facebook)
@@ -13,6 +33,6 @@ function addUser($email, $mal, $facebook = null) {
 require_once('libs/FirePHPCore/FirePHP.class.php');            
 $firephp = FirePHP::getInstance(true);
 $firephp->log($_POST);
-addUser($_POST['email'], $_POST['mal']);
+addUser($_POST['email'], $_POST['mal'], $_FILES[0]);
 
 ?>
