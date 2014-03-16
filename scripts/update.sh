b835c41df4ec92728b838e1e9c2161c2fcdc5eea
michboon <michboon@email>
2014-03-16 04:17:27 +0000

    update script

---
+++ scripts/update.sh
@@ -0,0 +1,12 @@
+rm -rf ./MALNotifier-master
+rm -f ./master.zip
+wget https://github.com/michboon/MALNotifier/archive/master.zip
+unzip master.zip
+sudo rm -rf /var/www/*
+sudo cp -r ./MALNotifier-master/www/* /var/www
+
+cd ./MALNotifier-master/scripts
+mysql -u root --password=root -e 'DROP DATABASE IF EXISTS MALNotifier;'
+mysql -u root --password=root < create_tables.sql
+
+sudo chmod -R 777 /var/www/scripts
